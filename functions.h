3341ee543595b8d4fc10ec24434e8153240bd3c8
cronolio <14379798+cronolio@users.noreply.github.com>
2024-04-26 10:38:57 +0000

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_DOT_H
+#define FUNCTIONS_DOT_H
+                     
+int search_in_categories(char *look);
+int get_variables(const char *func, char *look);
+int get_expression();
+int get_dep();
+
+#endif
